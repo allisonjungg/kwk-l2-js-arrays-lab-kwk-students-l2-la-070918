94d83a785b8c6727003c41fd22f5518e3a0a599e
allisonjungg <allisonjungg@gmail.com>
2018-07-13 23:21:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
 // Write your solution here!
-const drivers = ["Milo", "Otis", "Garfield"]
-function destructivelyAppendDriver(){
-  drivers.push("Ralph")
-}
-function destructivelyPrependDriver(){
-  drivers.unshift("Ralph")
-}
-function destructivelyRemoveLastDriver(){
-  drivers.pop()
-}
-function destructivelyRemoveFirstDriver(){
-  drivers.shift()
-}
-function appendDriverName(name){
-  var clone = drivers.slice(0)
-  clone.push(name)
-  return clone
-}
