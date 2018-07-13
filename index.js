4c9566a3d045ec15eb37836b3c0b89f223a1288d
allisonjungg <allisonjungg@gmail.com>
2018-07-13 23:22:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution here!
+function prependDriver (name){
+ var clone = drivers.slice(0)
+ clone.unshift(name)
+ return clone
+}
+function removeLastDriver (){
+ var clone = drivers.slice(0)
+ clone.splice(-1,1)
+ return clone
+}
+function removeFirstDriver (){
+ var clone = drivers.slice(0)
+ clone.shift(0)
+ return clone
+}
